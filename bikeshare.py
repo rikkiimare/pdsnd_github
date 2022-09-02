eacb05aee328b6e7f10654d70c2de7af76f7f2fb
Richard Irving <richard.irving3@dwp.gov.uk>
2022-09-02 09:21:12 +0100

    refactor: Adding comments to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
- 
+""" refactoring baby """
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -59,7 +59,7 @@ def get_filters():
     print('-'*40)
     return city, month, day
  
- 
+""" refactoring baby """
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
